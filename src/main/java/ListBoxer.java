9e734582b2d0a307e90380467335450f4b637e5f
Unknown <yulia16@inbox.ru>
2017-08-04 09:08:42 +0300

    04.08

--- src/main/java/ListBoxer.java
+++ src/main/java/ListBoxer.java
@@ -33,8 +33,8 @@ public class ListBoxer extends JFrame {
     private JCheckBox checkAlpha = new JCheckBox("Alphabetic", false);
     private JCheckBox checkNum = new JCheckBox("Numeric", false);
     private String[] rangeValuesAlpha = {"All", "<none>", "a-m", "n-z"};
-    private String[] rangeValuesNum = { "All","<none>", "0-100", "101-200", "201-300", "301-9999"};
-    private String[] rangeValuesAll = { "All","<none>", "0-100", "101-200", "201-300", "301-9999","a-m", "n-z"};
+    private String[] rangeValuesNum = {"All", "<none>", "0-100", "101-200", "201-300", "301-9999"};
+    private String[] rangeValuesAll = {"All", "<none>", "0-100", "101-200", "201-300", "301-9999", "a-m", "n-z"};
     private JComboBox rangeList = new JComboBox(rangeValuesAlpha);
 
     public ListBoxer() {
@@ -145,34 +145,50 @@ public class ListBoxer extends JFrame {
     public class AddButtonClick implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             if (!input.getText().equals("")) {
-                text.append(input.getText() + "\n");
-                recordsList.add(input.getText());
-                input.setText("");
-                recordsTotalCount.setText(String.valueOf(recordsList.size()));
-                recordsCountInList.setText(String.valueOf(text.getLineCount()-1));
+                try {
+                    Integer x = Integer.parseInt(input.getText());
+                    if (x<=10000) addText();
+                    else {
+                        JOptionPane.showMessageDialog(null, "Value is wrong" );
+                    }
+                }
+                catch (Exception ex){
+                    addText();
+                }
+
             }
         }
+    }
+
+    private void addText(){
+        text.append(input.getText() + "\n");
+        recordsList.add(input.getText());
+        input.setText("");
+        recordsTotalCount.setText(String.valueOf(recordsList.size()));
+        recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
     }
 
     public class SaveButton implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
             fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("List Boxer files (*.lbx)","lbx");
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("List Boxer files (*.lbx)", "lbx");
             fileChooser.setFileFilter(filter);
             if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
                 File file = fileChooser.getSelectedFile();
                 // save to file
-                String outputText = text.getText(); // строка для записи
-                try
-                {
-                    FileOutputStream fos=new FileOutputStream(file);
+                StringBuilder builder = new StringBuilder();
+                for (String x : recordsList) {
+                    builder.append(x).append("\n");
+                }
+                //String outputText = text.getText(); // строка для записи
+                try {
+                    FileOutputStream fos = new FileOutputStream(file);
                     // перевод строки в байты
-                    byte[] buffer = outputText.getBytes();
+                    byte[] buffer = builder.toString().getBytes();
 
                     fos.write(buffer, 0, buffer.length);
-                }
-                catch(Exception ex){
+                } catch (Exception ex) {
 
                     System.out.println(ex.getMessage());
                 }
@@ -185,7 +201,7 @@ public class ListBoxer extends JFrame {
         public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
             fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("List Boxer files (*.lbx)","lbx");
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("List Boxer files (*.lbx)", "lbx");
             fileChooser.setFileFilter(filter);
             if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 File file = fileChooser.getSelectedFile();
@@ -238,19 +254,48 @@ public class ListBoxer extends JFrame {
                 input.setTextLengthLimit(8);
                 input.setSymbols("alpha");
                 rangeList.setModel(new DefaultComboBoxModel(rangeValuesAlpha));
+                StringBuilder newText = new StringBuilder();
+                for (String x : recordsList) {
+                    try {
+                        Integer.parseInt(x);
+                    } catch (Exception ex) {
+                        newText.append(x).append("\n");
+                    }
+                }
+                text.setText(newText.toString());
+                recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
             }
             if (!checkAlpha.isSelected() && checkNum.isSelected()) {
                 //input.setTextLengthLimit(4);
                 input.setSymbols("num");
                 rangeList.setModel(new DefaultComboBoxModel(rangeValuesNum));
+                StringBuilder newText = new StringBuilder();
+                for (String x : recordsList) {
+                    try {
+                        Integer.parseInt(x);
+                        newText.append(x).append("\n");
+                    } catch (Exception ex) {
+
+                    }
+                }
+                text.setText(newText.toString());
+                recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
             }
             if (!checkAlpha.isSelected() && !checkNum.isSelected()) {
                 input.setEnabled(false);
                 input.setText("");
+                text.setText("");
             }
             if (checkAlpha.isSelected() && checkNum.isSelected()) {
-               input.setSymbols("all");
+                input.setTextLengthLimit(8);
+                input.setSymbols("all");
                 rangeList.setModel(new DefaultComboBoxModel(rangeValuesAll));
+                StringBuilder newText = new StringBuilder();
+                for (String x : recordsList) {
+                    newText.append(x).append("\n");
+                }
+                text.setText(newText.toString());
+                recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
             }
 
         }
@@ -263,120 +308,159 @@ public class ListBoxer extends JFrame {
                 //input.setTextLengthLimit(4);
                 input.setSymbols("num");
                 rangeList.setModel(new DefaultComboBoxModel(rangeValuesNum));
+                StringBuilder newText = new StringBuilder();
+                for (String x : recordsList) {
+                    try {
+                        Integer.parseInt(x);
+                        newText.append(x).append("\n");
+                    } catch (Exception ex) {
+
+                    }
+                }
+                text.setText(newText.toString());
+                recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
             }
             if (!checkNum.isSelected() && checkAlpha.isSelected()) {
                 input.setTextLengthLimit(8);
                 input.setSymbols("alpha");
                 rangeList.setModel(new DefaultComboBoxModel(rangeValuesAlpha));
-            }
-            if (!checkAlpha.isSelected() && !checkNum.isSelected()) {
-                input.setEnabled(false);
-                input.setText("");
-            }
-            if (checkAlpha.isSelected() && checkNum.isSelected()) {
-                input.setSymbols("all");
-                rangeList.setModel(new DefaultComboBoxModel(rangeValuesAll));
-            }
-
-            }
-
-        }
-
-    public class SelectDesc implements ItemListener {
-        public void itemStateChanged(ItemEvent e) {
-            if (e.getStateChange() == ItemEvent.SELECTED) {
-                String curList = text.getText();
-                String[] curListArray = curList.split("\n");
-                Arrays.sort(curListArray, Collections.reverseOrder());
-                StringBuilder sortedText = new StringBuilder();
-                for (String x : curListArray) {
-                    sortedText.append(x).append("\n");
+                StringBuilder newText = new StringBuilder();
+                for (String x : recordsList) {
+                    try {
+                        Integer.parseInt(x);
+                    } catch (Exception ex) {
+                        newText.append(x).append("\n");
+                    }
                 }
-                text.setText(sortedText.toString());
+                text.setText(newText.toString());
+                recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
             }
-        }
-
-    }
-
-    public class SelectAsc implements ItemListener {
-        public void itemStateChanged(ItemEvent e) {
-            if (e.getStateChange() == ItemEvent.SELECTED) {
-                String curList = text.getText();
-                String[] curListArray = curList.split("\n");
-                Arrays.sort(curListArray);
-                StringBuilder sortedText = new StringBuilder();
-                for (String x : curListArray) {
-                    sortedText.append(x).append("\n");
-                }
-                text.setText(sortedText.toString());
-            }
-        }
-
-    }
-
-    public class ChangeRange implements ItemListener {
-        public void itemStateChanged(ItemEvent e) {
-            if (e.getStateChange() == ItemEvent.SELECTED) {
-                Object item = e.getItem();
-                if (item.toString().equals("<none>")){
+                if (!checkAlpha.isSelected() && !checkNum.isSelected()) {
+                    input.setEnabled(false);
+                    input.setText("");
                     text.setText("");
-                    recordsCountInList.setText(String.valueOf(text.getLineCount()-1));
                 }
-                if (item.toString().equals("All")){
+                if (checkAlpha.isSelected() && checkNum.isSelected()) {
+                    input.setTextLengthLimit(8);
+                    input.setSymbols("all");
+                    rangeList.setModel(new DefaultComboBoxModel(rangeValuesAll));
                     StringBuilder newText = new StringBuilder();
                     for (String x : recordsList) {
                         newText.append(x).append("\n");
                     }
                     text.setText(newText.toString());
-                    recordsCountInList.setText(String.valueOf(text.getLineCount()-1));
+                    recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
                 }
-                if (item.toString().equals("0-100")){
-                    applyRangeNum(0,100);
-                }
-                if (item.toString().equals("101-200")){
-                    applyRangeNum(101,200);
-                }
-                if (item.toString().equals("201-300")){
-                    applyRangeNum(201,300);
-                }
-                if (item.toString().equals("301-9999")){
-                    applyRangeNum(301,9999);
-                }
-                if (item.toString().equals("a-m")){
-                    Pattern p = Pattern.compile("^[a-mA-M]");
-                    applyRangeAlpha(p);
-                }
-                if (item.toString().equals("n-z")){
-                    Pattern p = Pattern.compile("^[n-zN-Z]");
-                    applyRangeAlpha(p);
+
+            }
+
+        }
+
+
+        public class SelectDesc implements ItemListener {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String curList = text.getText();
+                    String[] curListArray = curList.split("\n");
+                    Arrays.sort(curListArray, Collections.reverseOrder());
+                    String temp = curListArray[curListArray.length-1];
+                    curListArray[curListArray.length-1] = curListArray[curListArray.length-2];
+                    curListArray[curListArray.length-2] = temp;
+                    StringBuilder sortedText = new StringBuilder();
+                    for (String x : curListArray) {
+                        sortedText.append(x).append("\n");
+                    }
+                    text.setText(sortedText.toString());
                 }
             }
+
         }
 
-    }
-
-    private void applyRangeNum(int min, int max){
-        StringBuilder newText = new StringBuilder();
-        for (String x : recordsList) {
-            if (Integer.parseInt(x) >= min && Integer.parseInt(x) <=max) {
-                newText.append(x).append("\n");
+        public class SelectAsc implements ItemListener {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String curList = text.getText();
+                    String[] curListArray = curList.split("\n");
+                    Arrays.sort(curListArray);
+                    StringBuilder sortedText = new StringBuilder();
+                    for (String x : curListArray) {
+                        sortedText.append(x).append("\n");
+                    }
+                    text.setText(sortedText.toString());
+                }
             }
+
         }
-        text.setText(newText.toString());
-        recordsCountInList.setText(String.valueOf(text.getLineCount()-1));
+
+        public class ChangeRange implements ItemListener {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    Object item = e.getItem();
+                    if (item.toString().equals("<none>")) {
+                        text.setText("");
+                        recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
+                    }
+                    if (item.toString().equals("All")) {
+                        StringBuilder newText = new StringBuilder();
+                        for (String x : recordsList) {
+                            newText.append(x).append("\n");
+                        }
+                        text.setText(newText.toString());
+                        recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
+                    }
+                    if (item.toString().equals("0-100")) {
+                        applyRangeNum(0, 100);
+                    }
+                    if (item.toString().equals("101-200")) {
+                        applyRangeNum(101, 200);
+                    }
+                    if (item.toString().equals("201-300")) {
+                        applyRangeNum(201, 300);
+                    }
+                    if (item.toString().equals("301-9999")) {
+                        applyRangeNum(301, 9999);
+                    }
+                    if (item.toString().equals("a-m")) {
+                        Pattern p = Pattern.compile("^[a-mA-M]");
+                        applyRangeAlpha(p);
+                    }
+                    if (item.toString().equals("n-z")) {
+                        Pattern p = Pattern.compile("^[n-zN-Z]");
+                        applyRangeAlpha(p);
+                    }
+                }
+            }
+
+        }
+
+        private void applyRangeNum(int min, int max) {
+            StringBuilder newText = new StringBuilder();
+            for (String x : recordsList) {
+                try {
+                    if (Integer.parseInt(x) >= min && Integer.parseInt(x) <= max) {
+                        newText.append(x).append("\n");
+                    }
+                }
+                catch (Exception e){
+
+                }
+            }
+            text.setText(newText.toString());
+            recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
+        }
+
+        private void applyRangeAlpha(Pattern p) {
+            StringBuilder newText = new StringBuilder();
+            for (String x : recordsList) {
+                Matcher matcher = p.matcher(x);
+                if (matcher.find()) newText.append(x).append("\n");
+            }
+            text.setText(newText.toString());
+            recordsCountInList.setText(String.valueOf(text.getLineCount() - 1));
+        }
+
     }
 
-    private void applyRangeAlpha(Pattern p){
-        StringBuilder newText = new StringBuilder();
-        for (String x : recordsList) {
-            Matcher matcher = p.matcher(x);
-            if (matcher.find()) newText.append(x).append("\n");
-        }
-        text.setText(newText.toString());
-        recordsCountInList.setText(String.valueOf(text.getLineCount()-1));
-    }
-
-    }
 
 
 
